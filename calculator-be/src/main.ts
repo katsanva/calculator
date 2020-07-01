f0e034767facaeed9a6ee87268a3e72908ad90a0
katsanva <this.is.ftm@gmail.com>
2020-07-01 16:35:22 +0300

    feat: add input validation

--- calculator-be/src/main.ts
+++ calculator-be/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { SwaggerValidatorPipe } from './common/swagger-validator.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new SwaggerValidatorPipe(document));
 
   await app.listen(process.env.PORT || 3001);
 }
