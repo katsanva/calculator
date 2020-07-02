be0b90a300d57f5009056fa88ee8fe2e1410159b
katsanva <this.is.ftm@gmail.com>
2020-07-02 16:16:36 +0300

    chore: prepare for fe unit tests

--- calculator-fe/src/App.js
+++ calculator-fe/src/App.js
@@ -1,139 +1,21 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-
 import { calculate } from "./calculator.api";
-import {
-  DIVISION,
-  MULTIPLICATION,
-  MINUS,
-  PLUS,
-  ZERO,
-  DEFAULT_VALUE,
-  SEPARATOR,
-  RESULT,
-  SYMBOLS_LIMIT,
-  NAN_DISPLAY,
-} from "./constants";
 import { Calculator } from "./Calculator";
+import { useCalculatorHookFactory } from "./use-calculator-hook.factory";
 
-const setOperand = (acc, symbol, set) => {
-  if (acc === DEFAULT_VALUE) {
-    set(symbol);
-
-    return symbol;
-  }
-
-  if (acc === ZERO && symbol === ZERO) {
-    return acc;
-  }
-
-  if (acc.length >= SYMBOLS_LIMIT) {
-    return acc;
-  }
-
-  if (symbol === SEPARATOR && acc.indexOf(SEPARATOR) > -1) {
-    return acc;
-  }
-
-  set(acc + symbol);
-
-  return acc + symbol;
-};
-
-const defaults = (operand) => {
-  switch (operand) {
-    case DEFAULT_VALUE: {
-      return ZERO;
-    }
-    case NAN_DISPLAY: {
-      return ZERO;
-    }
-    default: {
-      return operand;
-    }
-  }
-};
+const useCalculatorHook = useCalculatorHookFactory(useState, calculate);
 
 function App() {
-  const [operation, setOperation] = useState(undefined);
-  const [leftOperand, setLeftOperand] = useState(DEFAULT_VALUE);
-  const [rightOperand, setRightOperand] = useState(DEFAULT_VALUE);
-  const [result, setResult] = useState(ZERO);
-  const [error, setError] = useState();
-
-  const handleCalculationError = (e) => {
-    setError(e.message);
-
-    setTimeout(() => setError(), 3000);
-
-    return { result: NAN_DISPLAY };
-  };
-
-  const onInput = (e) => {
-    const symbol = e.target.value;
-
-    if (result === NAN_DISPLAY && !operation && leftOperand === DEFAULT_VALUE) {
-      setLeftOperand(symbol);
-      setResult(symbol);
-
-      return;
-    }
-
-    if (operation) {
-      return setResult(setOperand(rightOperand, symbol, setRightOperand));
-    }
-
-    return setResult(setOperand(leftOperand, symbol, setLeftOperand));
-  };
-
-  const onOperation = async (e) => {
-    const next = e.target.value;
-
-    if (
-      result !== ZERO &&
-      result !== NAN_DISPLAY &&
-      leftOperand === DEFAULT_VALUE
-    ) {
-      setLeftOperand(result);
-    }
-
-    if (operation) {
-      const { result } = await calculate(
-        operation,
-        defaults(leftOperand),
-        defaults(rightOperand)
-      ).catch(handleCalculationError);
-
-      setLeftOperand(result);
-      setResult(result);
-      setRightOperand(DEFAULT_VALUE);
-    }
-
-    setOperation(next);
-  };
-
-  const onCalculate = async (e) => {
-    if (!operation) {
-      return;
-    }
-
-    const { result } = await calculate(
-      operation,
-      defaults(leftOperand),
-      defaults(rightOperand)
-    ).catch(handleCalculationError);
-
-    setLeftOperand(result);
-    setResult(result);
-  };
-
-  const onReset = () => {
-    setLeftOperand(DEFAULT_VALUE);
-    setResult(ZERO);
-    setRightOperand(DEFAULT_VALUE);
-    setOperation(undefined);
-  };
+  const {
+    onInput,
+    onOperation,
+    onCalculate,
+    onReset,
+    result,
+    error,
+  } = useCalculatorHook();
 
   const props = {
     onInput,
---
+++ calculator-fe/src/use-calculator-hook.factory.js
@@ -0,0 +1,140 @@
+import {
+  ZERO,
+  DEFAULT_VALUE,
+  SEPARATOR,
+  SYMBOLS_LIMIT,
+  NAN_DISPLAY,
+} from "./constants";
+
+const setOperand = (acc, symbol, set) => {
+  if (acc === DEFAULT_VALUE) {
+    set(symbol);
+
+    return symbol;
+  }
+
+  if (acc === ZERO && symbol === ZERO) {
+    return acc;
+  }
+
+  if (acc.length >= SYMBOLS_LIMIT) {
+    return acc;
+  }
+
+  if (symbol === SEPARATOR && acc.indexOf(SEPARATOR) > -1) {
+    return acc;
+  }
+
+  set(acc + symbol);
+
+  return acc + symbol;
+};
+
+const defaults = (operand) => {
+  switch (operand) {
+    case DEFAULT_VALUE: {
+      return ZERO;
+    }
+    case NAN_DISPLAY: {
+      return ZERO;
+    }
+    default: {
+      return operand;
+    }
+  }
+};
+
+export const useCalculatorHookFactory = (_useState, _calculate) =>
+  function useCalculatorHook() {
+    const [operation, setOperation] = _useState(undefined);
+    const [leftOperand, setLeftOperand] = _useState(DEFAULT_VALUE);
+    const [rightOperand, setRightOperand] = _useState(DEFAULT_VALUE);
+    const [result, setResult] = _useState(ZERO);
+    const [error, setError] = _useState();
+
+    const handleCalculationError = (e) => {
+      setError(e.message);
+
+      setTimeout(() => setError(), 3000);
+
+      return { result: NAN_DISPLAY };
+    };
+
+    const onInput = (e) => {
+      const symbol = e.target.value;
+
+      if (
+        result === NAN_DISPLAY &&
+        !operation &&
+        leftOperand === DEFAULT_VALUE
+      ) {
+        setLeftOperand(symbol);
+        setResult(symbol);
+
+        return;
+      }
+
+      if (operation) {
+        return setResult(setOperand(rightOperand, symbol, setRightOperand));
+      }
+
+      return setResult(setOperand(leftOperand, symbol, setLeftOperand));
+    };
+
+    const onOperation = async (e) => {
+      const next = e.target.value;
+
+      if (
+        result !== ZERO &&
+        result !== NAN_DISPLAY &&
+        leftOperand === DEFAULT_VALUE
+      ) {
+        setLeftOperand(result);
+      }
+
+      if (operation) {
+        const { result } = await _calculate(
+          operation,
+          defaults(leftOperand),
+          defaults(rightOperand)
+        ).catch(handleCalculationError);
+
+        setLeftOperand(result);
+        setResult(result);
+        setRightOperand(DEFAULT_VALUE);
+      }
+
+      setOperation(next);
+    };
+
+    const onCalculate = async (e) => {
+      if (!operation) {
+        return;
+      }
+
+      const { result } = await _calculate(
+        operation,
+        defaults(leftOperand),
+        defaults(rightOperand)
+      ).catch(handleCalculationError);
+
+      setLeftOperand(result);
+      setResult(result);
+    };
+
+    const onReset = () => {
+      setLeftOperand(DEFAULT_VALUE);
+      setResult(ZERO);
+      setRightOperand(DEFAULT_VALUE);
+      setOperation(undefined);
+    };
+
+    return {
+      onInput,
+      onOperation,
+      onCalculate,
+      onReset,
+      result,
+      error,
+    };
+  };
